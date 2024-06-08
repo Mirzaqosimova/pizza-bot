70af6bcbf75ddff9c3f41c5a4fe366b034a6562d
Mirzaqosimova Farzona <mirzaqosimovafarzona@gmail.com>
2024-06-08 14:27:43 +0500

    fix bot webhook

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { corsOptions } from './shared/cors.options';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   app.enableCors(corsOptions);
+  const bot = app.get(getBotToken());
+  app.use(bot.webhookCallback('/bot'));
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
   await app.listen(configService.get<number>('PORT'));
